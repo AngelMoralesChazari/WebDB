960f999db8778d7c3050b8f213b98c171c13b713
Angel Morales <artes23graficas@gmail.com>
2025-11-20 17:14:17 -0600

    Creacion de las subpaginas e implementacion y conexion con la base de datos

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ def home():
     # Renderiza templates/index.html
     return render_template("index.html")
 
+@app.route("/peliculas")
+def peliculas():
+    return render_template("peliculas.html")
+
+@app.route("/series")
+def series():
+    return render_template("series.html")
+
+@app.route("/cuenta")
+def cuenta():
+    return render_template("cuenta.html")
+
+from db import get_connection
+
+conn = get_connection()
+if conn:
+    print("✅ Conexión exitosa a SQL Server")
+    conn.close()
+else:
+    print("❌ No se pudo conectar")
 
 if __name__ == "__main__":
     # debug=True es opcional, ayuda a recargar solo
---
+++ db.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+def get_connection():
+    """
+    Crea y retorna una conexión a SQL Server.
+    Ajusta los parámetros según tu configuración.
+    """
+    try:
+        conn = pyodbc.connect(
+            'DRIVER={SQL Server};'
+            'SERVER=ANGEL;'  # o .\SQLEXPRESS si usas SQL Server Express
+            'DATABASE=Movies;'
+            'Trusted_Connection=yes;'  # Autenticación de Windows
+        )
+        return conn
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
